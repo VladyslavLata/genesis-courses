8d851a3815da2ed1d452b13bd4310d70bcffabe0
VladyslavLata <vladlatahtc@gmail.com>
2023-03-16 12:54:08 +0200

    add new token

--- src/API/API.js
+++ src/API/API.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
-let TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhNWFlNDRiMC1jM2U5LTQzMmUtODkzNi01N2JiZDVlNzAxYjMiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg3OTM1MjAsImV4cCI6MTY3OTY5MzUyMH0.kMM8DKKZ2qaGfBKz_RQhEv-_gk3IgiOlpccBYSc1Bzg';
+// const TOKEN =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhNWFlNDRiMC1jM2U5LTQzMmUtODkzNi01N2JiZDVlNzAxYjMiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg3OTM1MjAsImV4cCI6MTY3OTY5MzUyMH0.kMM8DKKZ2qaGfBKz_RQhEv-_gk3IgiOlpccBYSc1Bzg';
+
+const TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZmZiOTY0YS0yNmVjLTQ1MzgtOTJlZC01ODQwMDU2MmM4OWYiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg5NTk4NzYsImV4cCI6MTY3OTg1OTg3Nn0.Y3X5ZJU0bhh_-2k1uK28i1Q2vTxYsIzXZrYv4j6Ug8s';
 
 axios.defaults.baseURL = 'https://api.wisey.app/api/v1';
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`;
-axios.defaults.headers.common['Access-Control-Allow-Origin'] =
-  'https://vladyslavlata.github.io/genesis-courses';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'true';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] =
+//   'https://vladyslavlata.github.io/genesis-courses';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'true';
 // const addAuthHeader = token => {
 //   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 // };
