848e4761810b4aa88cab0ebdcb212aa77ade44cd
VladyslavLata <95927612+VladyslavLata@users.noreply.github.com>
2023-03-18 18:46:44 +0000

    Deploying to gh-pages from @ VladyslavLata/genesis-courses@6475081d194842811c731aa1e347e6f396997fc5 🚀

---
+++ static/js/222.d4382c8a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[222],{7222:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var o=t(808),a="NotFound_not-found__R9oP-",r=t(184),u=function(){return(0,r.jsx)("div",{className:a,children:(0,r.jsx)(o.D,{main:!0,tag:"h2",children:"Page not found!"})})}}}]);
+//# sourceMappingURL=222.d4382c8a.chunk.js.map
